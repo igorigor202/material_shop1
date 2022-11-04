8bf3c1a2d50c07363d635164b7bcd457c2cfb45b
igorigor202 <igorigorcs2@gmail.com>
2022-11-04 18:45:09 +0300

    Updates

---
+++ precache-manifest.136422848861313e978cb97870739915.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7aca7af2909a33036f6f",
+    "url": "/material_shop1/static/js/main.7aca7af2.chunk.js"
+  },
+  {
+    "revision": "d5ffa408e29e652c76c6",
+    "url": "/material_shop1/static/js/1.d5ffa408.chunk.js"
+  },
+  {
+    "revision": "bd7aaf150b8a28d2c868",
+    "url": "/material_shop1/static/js/runtime~main.bd7aaf15.js"
+  },
+  {
+    "revision": "27a4a5cda474bfcfca67e1ac03a5c247",
+    "url": "/material_shop1/index.html"
+  }
+];
